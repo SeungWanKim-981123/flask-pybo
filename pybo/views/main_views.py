f22f4a5e4446248f697f2f67a9f2710b84d07286
seungwan <swsj2369@hanmail.net>
2023-02-24 22:59:10 +0900

    로거 사용 오류 기록

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,url_for
+from flask import Blueprint,url_for,current_app
 
 from werkzeug.utils import redirect
 bp=Blueprint('main',__name__,url_prefix='/')
@@ -9,5 +9,6 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
+    current_app.logger.info("INFO 레벨로 출력")
     return redirect(url_for('question._list'))#그냥 루트 링크로 접속함녀 url_for함수가 url을 반환하고 거기로 이동.
 
